df73162960a83ccb2a63f2a13f8ffc16c203ac93
Amol N <amol64546@gmail.com>
2025-07-31 15:15:47 +0530

    Add generated project

---
+++ test-UuidCreator-2/src/main/java/Main.java
@@ -0,0 +1,69 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.openfunction.functions.HttpFunction;
+import dev.openfunction.functions.HttpRequest;
+import dev.openfunction.functions.HttpResponse;
+import dev.openfunction.functions.Routable;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.github.f4b6a3.uuid.UuidCreator;
+
+public class Main extends Routable implements HttpFunction {
+
+  private static final ObjectMapper mapper = new ObjectMapper();
+
+  @Override
+  public void service(HttpRequest request, HttpResponse response) throws Exception {
+    Object result;
+    int statusCode = 200;
+    String path = request.getPath();
+    try {
+      switch (path) {
+        case "/getTimeBased":
+          result = getTimeBased.getTimeBased();
+          break;
+        case "/getRandomBased":
+          result = getRandomBased.getRandomBased();
+          break;
+
+        default:
+          result = "Invalid endpoint: " + path;
+          statusCode = 404;
+          break;
+      }
+
+    } catch (Exception e) {
+      result = e.getMessage();
+      statusCode = 500;
+    }
+    sendJsonResponse(response, statusCode, result);
+  }
+
+  @Override
+  public String getPath() {
+    return "/*";
+  }
+
+  @Override
+  public String[] getMethods() {
+    return new String[]{"GET"};
+  };
+
+  private void sendJsonResponse(HttpResponse response, int statusCode, Object result)
+      throws IOException {
+    Map<String, Object> responseBody = new HashMap<>();
+    responseBody.put("statusCode", statusCode);
+    if (statusCode == 200) {
+      responseBody.put("status", "success");
+      responseBody.put("data", result);
+    } else {
+      responseBody.put("status", "error");
+      responseBody.put("message", result);
+    }
+
+    response.setStatusCode(statusCode);
+    response.setContentType("application/json");
+    response.getWriter().write(mapper.writeValueAsString(responseBody));
+  }
+}
