25b5936a66bc683f12e243ed8fd7bedac0cce2d3
vamshi1271 <vamshi.k@mobiusdtaas.ai>
2025-08-11 12:11:08 +0530

    Add generated project

---
+++ test-uuidcreator-2/src/main/java/Main.java
@@ -0,0 +1,56 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.openfunction.functions.HttpFunction;
+import dev.openfunction.functions.HttpRequest;
+import dev.openfunction.functions.HttpResponse;
+import dev.openfunction.functions.Routable;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import com.github.f4b6a3.uuid.UuidCreator;
+
+public class Main extends Routable implements HttpFunction {
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+    private static final Map<String, Function<Map<String, Object>, Object>> pathHandlers = new HashMap<>();
+
+    static {
+        pathHandlers.put("/getTimeBased", Main::getTimeBased);
+        pathHandlers.put("/getRandomBased", Main::getRandomBased);
+    }
+
+    private static Object getTimeBased(Map<String, Object> body) {
+        return UuidCreator.getTimeBased();
+    }
+    private static Object getRandomBased(Map<String, Object> body) {
+        return UuidCreator.getRandomBased();
+    }
+
+    @Override
+    public void service(HttpRequest request, HttpResponse response) throws Exception {
+        String requestBody = request.getReader().lines()
+                .collect(Collectors.joining(System.lineSeparator()));
+        Map<String, Object> body = mapper.readValue(requestBody, new TypeReference<>() {
+        });
+        Object result = pathHandlers.get(request.getPath())
+                .apply(body);
+        sendResponse(response, result);
+    }
+
+    @Override
+    public String getPath() {
+        return "/*";
+    }
+
+    private void sendResponse(HttpResponse response, Object result)
+            throws IOException {
+        Map<String, Object> responseBody = new HashMap<>();
+        responseBody.put("result", result);
+        responseBody.put("message", result);
+        response.setContentType("application/json");
+        response.getWriter().write(mapper.writeValueAsString(responseBody));
+    }
+}
