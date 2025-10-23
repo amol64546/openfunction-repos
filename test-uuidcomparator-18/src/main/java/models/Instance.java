b054bc886f3bb62bd8bc002e9aa165930d4f2c79
Amol N <amol64546@gmail.com>
2025-10-23 20:42:21 +0530

    Add generated project

---
+++ test-uuidcomparator-18/src/main/java/models/Instance.java
@@ -0,0 +1,53 @@
+package models;
+
+import enums.InstanceKind;
+
+import java.util.List;
+
+public class Instance {
+    private InstanceKind kind;
+    private String className;
+    private String name;
+    private String base64;
+    private List<Arg> args;
+
+    public String getBase64() {
+        return base64;
+    }
+
+    public void setBase64(String base64) {
+        this.base64 = base64;
+    }
+
+    public InstanceKind getKind() {
+        return kind;
+    }
+
+    public void setKind(InstanceKind kind) {
+        this.kind = kind;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Arg> getArgs() {
+        return args;
+    }
+
+    public void setArgs(List<Arg> args) {
+        this.args = args;
+    }
+}
